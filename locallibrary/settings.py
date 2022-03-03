c60093ed70c483bd3a35c5bd36f868dea840a02e
Gerardo Bolanos <Gbolanos@inductiveautomation.com>
2022-03-03 11:04:30 -0800

    update

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = ['sleepy-refuge-59680.herokuapp.com',
                  '127.0.0.1',
                  'mdn-library-app-alb-134616707.us-west-1.elb.amazonaws.com',
-                 'mdn-library-app-alb-134616707.us-west-1.elb.amazonaws.com'
+                 'mdn-library-app-alb-134616707.us-west-2.elb.amazonaws.com'
                 ]
 
 
